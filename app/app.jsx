930745809738bc160de67f85f0e6c71b901003e4
Alex Kiryxa <kiryxa@Alexs-iMac.local>
2017-01-13 14:37:44 +0200

    Initial commit

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 let {Route, Router, IndexRoute, hashHistory} = require('react-router');
+let TodoApp = require('TodoApp');
 
 // Load foundation
 $(document).foundation();
@@ -9,6 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <p>Boilerplate Project</p>,
+    <TodoApp/>,
     document.getElementById('app')
 );
